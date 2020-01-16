2050b7bf56e4d252968412dd7a35877c4a298055
shenry45 <shawn45henry@gmail.com>
2020-01-15 22:20:40 -0700

    exercise 00 through 12 complete

--- src/07_slices.py
+++ src/07_slices.py
@@ -8,30 +8,45 @@ overview: https://stackoverflow.com/a/509295
 
 Use Python's slice syntax to achieve the following:
 """
+import math
 
 a = [2, 4, 1, 7, 9, 6]
+b = [1, 3, 5, 10, 12, 30, 2000, 1926, 51655, 999999]
 
 # Output the second element: 4:
-print()
+find_4 = a.index(4)
+print(a[find_4:find_4 + 1])
 
 # Output the second-to-last element: 9
-print()
+print(a[len(a)-2:len(a)-1])
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+print(a[len(a)-3:])
 
 # Output the two middle elements in the array: [1, 7]
-print()
+# make fn to handle
+
+
+def get_middle_of_list(list_name):
+    middle_of_list = math.floor(len(list_name) / 2)
+    if middle_of_list % 2 == 0:
+        print(list_name[middle_of_list:middle_of_list + 1])
+    else:
+        print(list_name[middle_of_list - 1:middle_of_list + 1])
+
+
+get_middle_of_list(a)
+get_middle_of_list(b)
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+print(a[1:])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+print(a[:len(a)-1])
 
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print()
+print(s[7:12])
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -34,11 +34,22 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.append({
+    "lat": 100,
+    "lon": -30,
+    "name": "a fourth place"
+})
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
-# YOUR CODE HERE
+for obj in waypoints:
+    if obj["name"] == "a place":
+        obj["name"] = "not a real place"
+
+
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+for obj in waypoints:
+    print(f'%s, has a latitude of %d and longitude of %d' % (obj["name"].capitalize(), obj["lat"], obj["lon"]))
